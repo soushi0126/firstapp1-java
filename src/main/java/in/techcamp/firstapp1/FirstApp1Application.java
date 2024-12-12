cf8b5b0d670b10cf32b4d3cee11eea4fe7bded16
soushi0126 <sousi0126@gmail.com>
2024-12-12 23:09:51 +0900

    first commit

---
+++ src/main/java/in/techcamp/firstapp1/FirstApp1Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.firstapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApp1Application.class, args);
+	}
+
+}
