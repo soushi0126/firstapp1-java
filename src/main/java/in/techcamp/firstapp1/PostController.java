7743e07b7ff36528e3916b026061896defc7778f
soushi0126 <sousi0126@gmail.com>
2024-12-14 22:42:24 +0900

    controllerでhello.htmlを呼び出せるよう変更

--- src/main/java/in/techcamp/firstapp1/PostController.java
+++ src/main/java/in/techcamp/firstapp1/PostController.java
@@ -2,13 +2,11 @@ package in.techcamp.firstapp1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
     @GetMapping("/hello")
-    @ResponseBody
     public String showHello(){
-        return "<h1>Hello World!</h1>";
+        return "hello";
     }
 }
